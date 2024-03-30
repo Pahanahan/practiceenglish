ee3f09fd208ac36beb641a842e870ad414411a0d
Pahanahan <korgfd@yandex.ua>
2024-03-30 20:42:58 +0300

    first commmit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector(".header");
+
+document
+  .querySelector(".lesson__button")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    body.style.backgroundcolor = "white";
+  });
