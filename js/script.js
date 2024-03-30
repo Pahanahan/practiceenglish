62a8b6d90ab19e442280a598a376e2a4d4f4abd1
Pahanahan <korgfd@yandex.ua>
2024-03-30 23:10:44 +0300

    added first function ask

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const lesson1 = document.querySelector(".begginer1-lesson__box1-learn");
+const lessonStart = document.querySelector(".begginer1-lesson__box1-start");
 
-document
-  .querySelector(".begginer1-lesson__box1-start")
-  .addEventListener("click", function (event) {
+if (lesson1) {
+  document;
+  lessonStart.addEventListener("click", function (event) {
     event.preventDefault();
     lesson1.style.display = "flex";
   });
+}
