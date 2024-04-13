0241eff0862917a13490239007aa5cb4f4d1453a
Pahanahan <korgfd@yandex.ua>
2024-04-13 20:25:35 +0300

    rebuild href, src

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const lesson1 = document.querySelector(".begginer1-lesson__box1-learn");
-const lessonStart = document.querySelector(".begginer1-lesson__box1-start");
+// const lesson1 = document.querySelector(".begginer1-lesson__box1-learn");
+// const lessonStart = document.querySelector(".begginer1-lesson__box1-start");
 
-if (lesson1) {
-  document;
-  lessonStart.addEventListener("click", function (event) {
-    event.preventDefault();
-    lesson1.style.display = "flex";
-  });
-}
+// if (lesson1) {
+//   document;
+//   lessonStart.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     lesson1.style.display = "flex";
+//   });
+// }
