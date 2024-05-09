081c00d8054dd1ee7c4960c18d0d7a51009e4ee5
Pahanahan <korgfd@yandex.ua>
2024-05-09 15:09:21 +0300

    added to style.css new background-color to body

---
+++ lessons/beginner/lesson7/lessons/Новая папка/04.js
@@ -0,0 +1,426 @@
+const lesson01 = document.querySelector(".lesson01");
+const lesson02 = document.querySelector(".lesson02");
+const lesson03 = document.querySelector(".lesson03");
+const lesson04 = document.querySelector(".lesson04");
+const lesson05 = document.querySelector(".lesson05");
+const lesson06 = document.querySelector(".lesson06");
+const lesson07 = document.querySelector(".lesson07");
+const lesson08 = document.querySelector(".lesson08");
+const lesson09 = document.querySelector(".lesson09");
+const lesson10 = document.querySelector(".lesson10");
+const lesson11 = document.querySelector(".lesson11");
+const lesson12 = document.querySelector(".lesson12");
+
+const stringChoice = document.querySelector(".lesson-learn__block-str");
+const stringChoice2 = document.querySelector(".lesson-learn__block-str2");
+const stringChoice3 = document.querySelector(".lesson-learn__block-str3");
+const stringChoice4 = document.querySelector(".lesson-learn__block-str4");
+const stringChoice5 = document.querySelector(".lesson-learn__block-str5");
+const stringChoice6 = document.querySelector(".lesson-learn__block-str6");
+const stringChoice7 = document.querySelector(".lesson-learn__block-str7");
+const stringChoice8 = document.querySelector(".lesson-learn__block-str8");
+const stringChoice9 = document.querySelector(".lesson-learn__block-str9");
+const stringChoice10 = document.querySelector(".lesson-learn__block-str10");
+const stringChoice11 = document.querySelector(".lesson-learn__block-str11");
+const stringChoice12 = document.querySelector(".lesson-learn__block-str12");
+const stringChoice13 = document.querySelector(".lesson-learn__block-str13");
+const stringChoice14 = document.querySelector(".lesson-learn__block-str14");
+const stringChoice15 = document.querySelector(".lesson-learn__block-str15");
+const stringChoice16 = document.querySelector(".lesson-learn__block-str16");
+const stringChoice17 = document.querySelector(".lesson-learn__block-str17");
+const stringChoice18 = document.querySelector(".lesson-learn__block-str18");
+const stringChoice19 = document.querySelector(".lesson-learn__block-str19");
+const stringChoice20 = document.querySelector(".lesson-learn__block-str20");
+const stringChoice21 = document.querySelector(".lesson-learn__block-str21");
+const stringChoice22 = document.querySelector(".lesson-learn__block-str22");
+const stringChoice23 = document.querySelector(".lesson-learn__block-str23");
+const stringChoice24 = document.querySelector(".lesson-learn__block-str24");
+const stringChoice25 = document.querySelector(".lesson-learn__block-str25");
+const stringChoice26 = document.querySelector(".lesson-learn__block-str26");
+const stringChoice27 = document.querySelector(".lesson-learn__block-str27");
+const stringChoice28 = document.querySelector(".lesson-learn__block-str28");
+const stringChoice29 = document.querySelector(".lesson-learn__block-str29");
+const stringChoice30 = document.querySelector(".lesson-learn__block-str30");
+const stringChoice31 = document.querySelector(".lesson-learn__block-str31");
+const stringChoice32 = document.querySelector(".lesson-learn__block-str32");
+const stringChoice33 = document.querySelector(".lesson-learn__block-str33");
+const stringChoice34 = document.querySelector(".lesson-learn__block-str34");
+const stringChoice35 = document.querySelector(".lesson-learn__block-str35");
+const stringChoice36 = document.querySelector(".lesson-learn__block-str36");
+const stringChoice37 = document.querySelector(".lesson-learn__block-str37");
+const stringChoice38 = document.querySelector(".lesson-learn__block-str38");
+const stringChoice39 = document.querySelector(".lesson-learn__block-str39");
+const stringChoice40 = document.querySelector(".lesson-learn__block-str40");
+const stringChoice41 = document.querySelector(".lesson-learn__block-str41");
+const stringChoice42 = document.querySelector(".lesson-learn__block-str42");
+const stringChoice43 = document.querySelector(".lesson-learn__block-str43");
+const stringChoice44 = document.querySelector(".lesson-learn__block-str44");
+const stringChoice45 = document.querySelector(".lesson-learn__block-str45");
+const stringChoice46 = document.querySelector(".lesson-learn__block-str46");
+const stringChoice47 = document.querySelector(".lesson-learn__block-str47");
+const stringChoice48 = document.querySelector(".lesson-learn__block-str48");
+const stringChoice49 = document.querySelector(".lesson-learn__block-str49");
+const stringChoice50 = document.querySelector(".lesson-learn__block-str50");
+const stringChoice51 = document.querySelector(".lesson-learn__block-str51");
+const stringChoice52 = document.querySelector(".lesson-learn__block-str52");
+const stringChoice53 = document.querySelector(".lesson-learn__block-str53");
+const stringChoice54 = document.querySelector(".lesson-learn__block-str54");
+const stringChoice55 = document.querySelector(".lesson-learn__block-str55");
+const stringChoice56 = document.querySelector(".lesson-learn__block-str56");
+const stringChoice57 = document.querySelector(".lesson-learn__block-str57");
+
+const checkPhrase = document.querySelector(".lesson-learn__check");
+const checkPhrase2 = document.querySelector(".lesson-learn__check2");
+const checkPhrase3 = document.querySelector(".lesson-learn__check3");
+const checkPhrase4 = document.querySelector(".lesson-learn__check4");
+const checkPhrase5 = document.querySelector(".lesson-learn__check5");
+const checkPhrase6 = document.querySelector(".lesson-learn__check6");
+const checkPhrase7 = document.querySelector(".lesson-learn__check7");
+const checkPhrase8 = document.querySelector(".lesson-learn__check8");
+const checkPhrase9 = document.querySelector(".lesson-learn__check9");
+const checkPhrase10 = document.querySelector(".lesson-learn__check10");
+const checkPhrase11 = document.querySelector(".lesson-learn__check11");
+const checkPhrase12 = document.querySelector(".lesson-learn__check12");
+
+const nextBtn = document.querySelector(".lesson-learn__next");
+const nextBtn2 = document.querySelector(".lesson-learn__next2");
+const nextBtn3 = document.querySelector(".lesson-learn__next3");
+const nextBtn4 = document.querySelector(".lesson-learn__next4");
+const nextBtn5 = document.querySelector(".lesson-learn__next5");
+const nextBtn6 = document.querySelector(".lesson-learn__next6");
+const nextBtn7 = document.querySelector(".lesson-learn__next7");
+const nextBtn8 = document.querySelector(".lesson-learn__next8");
+const nextBtn9 = document.querySelector(".lesson-learn__next9");
+const nextBtn10 = document.querySelector(".lesson-learn__next10");
+const nextBtn11 = document.querySelector(".lesson-learn__next11");
+const nextBtn12 = document.querySelector(".lesson-learn__next12");
+
+const answerRightOrNot = document.querySelector(".lesson-learn__box-answer");
+const answerRightOrNot2 = document.querySelector(".lesson-learn__box-answer2");
+const answerRightOrNot3 = document.querySelector(".lesson-learn__box-answer3");
+const answerRightOrNot4 = document.querySelector(".lesson-learn__box-answer4");
+const answerRightOrNot5 = document.querySelector(".lesson-learn__box-answer5");
+const answerRightOrNot6 = document.querySelector(".lesson-learn__box-answer6");
+const answerRightOrNot7 = document.querySelector(".lesson-learn__box-answer7");
+const answerRightOrNot8 = document.querySelector(".lesson-learn__box-answer8");
+const answerRightOrNot9 = document.querySelector(".lesson-learn__box-answer9");
+const answerRightOrNot10 = document.querySelector(
+  ".lesson-learn__box-answer10"
+);
+const answerRightOrNot11 = document.querySelector(
+  ".lesson-learn__box-answer11"
+);
+const answerRightOrNot12 = document.querySelector(
+  ".lesson-learn__box-answer12"
+);
+
+const boxLessonsStrings = document.querySelector(
+  ".lesson-learn__block-strings"
+);
+const boxLessonsStrings2 = document.querySelector(
+  ".lesson-learn__block-strings2"
+);
+const boxLessonsStrings3 = document.querySelector(
+  ".lesson-learn__block-strings3"
+);
+const boxLessonsStrings4 = document.querySelector(
+  ".lesson-learn__block-strings4"
+);
+const boxLessonsStrings5 = document.querySelector(
+  ".lesson-learn__block-strings5"
+);
+const boxLessonsStrings6 = document.querySelector(
+  ".lesson-learn__block-strings6"
+);
+const boxLessonsStrings7 = document.querySelector(
+  ".lesson-learn__block-strings7"
+);
+const boxLessonsStrings8 = document.querySelector(
+  ".lesson-learn__block-strings8"
+);
+const boxLessonsStrings9 = document.querySelector(
+  ".lesson-learn__block-strings9"
+);
+const boxLessonsStrings10 = document.querySelector(
+  ".lesson-learn__block-strings10"
+);
+const boxLessonsStrings11 = document.querySelector(
+  ".lesson-learn__block-strings11"
+);
+const boxLessonsStrings12 = document.querySelector(
+  ".lesson-learn__block-strings12"
+);
+
+const lessonBoxStringsAnswer1 = document.querySelector(
+  ".lesson-learn__block-strings-answer1"
+);
+const lessonBoxStringsAnswer2 = document.querySelector(
+  ".lesson-learn__block-strings-answer2"
+);
+const lessonBoxStringsAnswer3 = document.querySelector(
+  ".lesson-learn__block-strings-answer3"
+);
+const lessonBoxStringsAnswer4 = document.querySelector(
+  ".lesson-learn__block-strings-answer4"
+);
+const lessonBoxStringsAnswer5 = document.querySelector(
+  ".lesson-learn__block-strings-answer5"
+);
+const lessonBoxStringsAnswer6 = document.querySelector(
+  ".lesson-learn__block-strings-answer6"
+);
+
+const stringsArray = [
+  stringChoice,
+  stringChoice2,
+  stringChoice3,
+];
+const stringsArray2 = [
+  stringChoice4,
+  stringChoice5,
+  stringChoice6,
+  stringChoice7,
+];
+const stringsArray3 = [
+  stringChoice8,
+  stringChoice9,
+  stringChoice10,
+  stringChoice11,
+];
+const stringsArray4 = [
+  stringChoice12,
+  stringChoice13,
+  stringChoice14,
+  stringChoice15,
+];
+const stringsArray5 = [
+  stringChoice16,
+  stringChoice17,
+  stringChoice18,
+  stringChoice19,
+];
+const stringsArray6 = [
+  stringChoice20,
+  stringChoice21,
+  stringChoice22,
+  stringChoice23,
+  stringChoice24,
+  stringChoice25,
+  stringChoice26,
+  stringChoice27,
+  stringChoice28,
+];
+
+const stringsArrays = [
+  stringsArray,
+  stringsArray2,
+  stringsArray3,
+  stringsArray4,
+  stringsArray5,
+  stringsArray6,
+];
+
+const boxLessonsStringsArray = [
+  boxLessonsStrings,
+  boxLessonsStrings2,
+  boxLessonsStrings3,
+  boxLessonsStrings4,
+  boxLessonsStrings5,
+  boxLessonsStrings6,
+];
+
+const lessonBoxStringsAnswersArray = [
+  lessonBoxStringsAnswer1,
+  lessonBoxStringsAnswer2,
+  lessonBoxStringsAnswer3,
+  lessonBoxStringsAnswer4,
+  lessonBoxStringsAnswer5,
+  lessonBoxStringsAnswer6,
+];
+
+const audio01 = new Audio("audio04/they_don't_agree.wav");
+const audio02 = new Audio("audio04/i_don't_make_mistakes.wav");
+const audio03 = new Audio("audio04/i_don't_do_business.wav");
+const audio04 = new Audio("audio04/i_pay_for_it.wav");
+const audio05 = new Audio("audio04/they_don't_play_football.wav");
+const audio06 = new Audio("audio04/i_don't_want_to_spend_my_money_on_it.wav");
+
+const answersArray = [
+"They don't agree",
+"I don't make mistakes",
+"I don't do business",
+"I pay for it",
+"They don't play football",
+"I don't want to spend my money on it"
+];
+
+let rightAnswers = 0;
+
+let joinedText;
+
+let beginner06 = JSON.parse(localStorage.getItem("beginner06"));
+
+for (let i = 0; i < stringsArrays.length; i++) {
+  const stringsArray = stringsArrays[i];
+  const boxLessonsStrings = boxLessonsStringsArray[i];
+  const lessonBoxStringsAnswer = lessonBoxStringsAnswersArray[i];
+
+  stringsArray.forEach((stringChoice) => {
+    stringChoice.addEventListener("click", function () {
+      pasteInPhrase(stringChoice, boxLessonsStrings, lessonBoxStringsAnswer);
+    });
+  });
+}
+
+function pasteInPhrase(
+  stringChoice,
+  boxLessonsStrings,
+  lessonBoxStringsAnswer
+) {
+  if (stringChoice.classList.contains("str__answer")) {
+    boxLessonsStrings.appendChild(stringChoice);
+    stringChoice.classList.remove("str__answer");
+  } else {
+    lessonBoxStringsAnswer.appendChild(stringChoice);
+    stringChoice.classList.add("str__answer");
+  }
+}
+
+function checkPhrases(
+  lessonBoxStringsAnswer,
+  lessonCheck,
+  index,
+  answerRightOrNot,
+  nextLesson,
+  audio
+) {
+  const divElements = document.querySelectorAll(
+    `.lesson-learn__block-strings-answer${
+      index + 1
+    } [class^="lesson-learn__block-str"]`
+  );
+  const texts = Array.from(divElements).map((element) => element.textContent);
+  joinedText = texts.join(" ");
+
+  if (lessonBoxStringsAnswer.textContent.length > 0) {
+    if (
+      joinedText === answersArray[index]
+    ) {
+      lessonCheck.style.display = "none";
+      nextLesson.style.display = "block";
+      answerRightOrNot.style.display = "flex";
+      answerRightOrNot.classList.add("lesson-learn__box-answer__correct");
+      answerRightOrNot.textContent = "Правильно!";
+      audio.play();
+      rightAnswers++;
+    } else {
+      lessonCheck.style.display = "none";
+      nextLesson.style.display = "block";
+      answerRightOrNot.style.display = "flex";
+      answerRightOrNot.classList.add("lesson-learn__box-answer__incorrect");
+      answerRightOrNot.textContent = `Правильный ответ:\n ${answersArray[index]}`;
+      audio.play();
+    }
+  }
+}
+
+function lessonNext(lesson1, lesson2) {
+  lesson1.style.display = "none";
+  lesson2.style.display = "flex";
+}
+
+checkPhrase.addEventListener("click", function () {
+  checkPhrases(
+    lessonBoxStringsAnswer1,
+    checkPhrase,
+    0,
+    answerRightOrNot,
+    nextBtn,
+    audio01
+  );
+});
+
+nextBtn.addEventListener("click", function () {
+  lessonNext(lesson01, lesson02);
+});
+
+checkPhrase2.addEventListener("click", function () {
+  checkPhrases(
+    lessonBoxStringsAnswer2,
+    checkPhrase2,
+    1,
+    answerRightOrNot2,
+    nextBtn2,
+    audio02
+  );
+});
+
+nextBtn2.addEventListener("click", function () {
+  lessonNext(lesson02, lesson03);
+});
+
+checkPhrase3.addEventListener("click", function () {
+  checkPhrases(
+    lessonBoxStringsAnswer3,
+    checkPhrase3,
+    2,
+    answerRightOrNot3,
+    nextBtn3,
+    audio03
+  );
+});
+
+nextBtn3.addEventListener("click", function () {
+  lessonNext(lesson03, lesson04);
+});
+
+checkPhrase4.addEventListener("click", function () {
+  checkPhrases(
+    lessonBoxStringsAnswer4,
+    checkPhrase4,
+    3,
+    answerRightOrNot4,
+    nextBtn4,
+    audio04
+  );
+});
+
+nextBtn4.addEventListener("click", function () {
+  lessonNext(lesson04, lesson05);
+});
+
+checkPhrase5.addEventListener("click", function () {
+  checkPhrases(
+    lessonBoxStringsAnswer5,
+    checkPhrase5,
+    4,
+    answerRightOrNot5,
+    nextBtn5,
+    audio05
+  );
+});
+
+nextBtn5.addEventListener("click", function () {
+  lessonNext(lesson05, lesson06);
+});
+
+checkPhrase6.addEventListener("click", function () {
+  checkPhrases(
+    lessonBoxStringsAnswer6,
+    checkPhrase6,
+    5,
+    answerRightOrNot6,
+    nextBtn6,
+    audio06
+  );
+  if (rightAnswers === 6) {
+    if (beginner06.length === 3) {
+      beginner06.push(4);
+      saveToLocalStorage();
+    }
+  }
+});
+
+const saveToLocalStorage = function () {
+  localStorage.setItem("beginner06", JSON.stringify(beginner06));
+};
